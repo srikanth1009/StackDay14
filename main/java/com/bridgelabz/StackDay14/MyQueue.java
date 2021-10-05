2c5a4236ef1391e1862f56540b0df7ec68c135f6
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-05 18:19:26 +0530

    Added:Ability to create Queue

---
+++ main/java/com/bridgelabz/StackDay14/MyQueue.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.StackDay14;
+
+public class MyQueue<K> {
+		public final MyLinkedList myLinkedList;
+
+		public MyQueue() {
+			this.myLinkedList = new MyLinkedList();
+
+		}
+
+		public void enqueue(INode element) {
+			myLinkedList.append(element);
+		}
+
+		public INode peak() {
+			return myLinkedList.head;
+		}
+
+		public void printQueue() {
+			myLinkedList.display();
+		}
+
+}
+
